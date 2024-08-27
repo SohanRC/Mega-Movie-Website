d6dab1f2fe2f7e3ca2a8d1295f2890c6cb76a79b
Surajit Maity <sibu23122003@gmail.com>
2024-08-27 20:20:18 +0530

    reservation frontend

--- Client/src/pages/index.js
+++ Client/src/pages/index.js
@@ -4,5 +4,7 @@ import SignInPage from "./SignInPage";
 import SignUpPage from "./SignUpPage";
 import PasswordResetPage from "./PasswordResetPage";
 import Theaters from "../components/DashBoard/Theaters";
+import ReservationPage from './reservation';
+import TicketBooking from "./ticketBook";
 
-export { HomePage, SignInPage, SignUpPage, PasswordResetPage ,Movies,Theaters}
+export { HomePage, SignInPage, SignUpPage, PasswordResetPage ,Movies,Theaters,ReservationPage,TicketBooking}
