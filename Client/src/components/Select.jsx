8ab6e10c493144cb916012c051c29f87a47256dd
Siddhant <sidsaiba2703@gmail.com>
2024-09-02 02:30:06 +0530

    location filter

--- Client/src/components/Select.jsx
+++ Client/src/components/Select.jsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, useId } from "react";
 
-function Select({ options, label, className = "", ...props }, ref) {
+function Select({ options, label,className = "", ...props }, ref) {
   // console.log(options);
   const id = useId();
   return (
@@ -10,6 +10,7 @@ function Select({ options, label, className = "", ...props }, ref) {
         {...props}
         id={id}
         ref={ref}
+        defaultValue="State"
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
         {options?.map((option) => (
--- Client/src/components/Select/State.jsx
+++ Client/src/components/Select/State.jsx
@@ -1,77 +1,45 @@
-import {
-  Combobox,
-  ComboboxButton,
-  ComboboxInput,
-  ComboboxOption,
-  ComboboxOptions,
-} from "@headlessui/react";
-import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useId } from "react";
+import { City, State as IndianState } from "country-state-city";
+import Select from "../Select.jsx";
+import { useDispatch } from "react-redux";
 import { setState } from "../../store/LocationSlice.js";
 
-export default function SelectState({ data, selected, setSelected }) {
-  
-  const [query, setQuery] = useState("");
+function State({ data, selected, setSelected, className = "", setCities }) {
+  // console.log(data);
+
+  const id = useId();
   const dispatch = useDispatch();
 
-  const filteredData =
-    query === ""
-      ? data
-      : data.filter((currData) => {
-          return currData.name.toLowerCase().includes(query.toLowerCase());
-        });
+  async function handleChange(e) {
+    const currState = await data.filter((curr) => curr.name === e.target.value);
+    dispatch(setState(currState));
+    // console.log(currState);
 
-  const filterState = ()=>{
-    // console.log(selected);
-    dispatch(setState(selected));
-    
+    const cities = City.getCitiesOfState("IN", currState[0].isoCode);
+    setCities(cities);
+    // console.log(cities);
+    dispatch(setState(currState[0].name));
   }
-  filterState();
-
 
   return (
-    <div className="z-50">
-      <Combobox
-        value={selected}
-        onChange={(value) => setSelected(value)}
-        onClose={() => setQuery("")}
+    <div className="flex flex-col gap-2">
+      <select
+        id={id}
+        defaultValue="State"
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        onChange={(e) => {
+          handleChange(e);
+          setSelected(e.target.value);
+        }}
       >
-        <div className="relative">
-          <ComboboxInput
-            className={clsx(
-              "w-full rounded-lg border-none bg-white py-1.5 pr-8 pl-3 text-sm/6 text-black",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
-            )}
-            displayValue={(data) => data?.name}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
-            <ChevronDownIcon className="size-4 fill-black/60 group-data-[hover]:fill-black/100" />
-          </ComboboxButton>
-        </div>
-
-        <ComboboxOptions
-          anchor="bottom"
-          transition
-          className={clsx(
-            "w-[var(--input-width)] rounded-xl border  bg-white p-1 [--anchor-gap:var(--spacing-1)] empty:invisible z-40  border-white",
-            "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0 h-[15rem]"
-          )}
-        >
-          {filteredData?.map((currData) => (
-            <ComboboxOption
-              key={currData.id}
-              value={currData}
-              className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
-            >
-              <CheckIcon className="invisible size-4 fill-white group-data-[selected]:visible" />
-              <div className="text-sm/6 text-black">{currData.name}</div>
-            </ComboboxOption>
-          ))}
-        </ComboboxOptions>
-      </Combobox>
+        {data?.map((option, index) => (
+          <option key={index} value={option.name}>
+            {option.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
+
+export default State;
